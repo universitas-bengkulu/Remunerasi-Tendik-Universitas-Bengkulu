0d20363ecff0fe5f4d95d1e852fefe0e7f72e7d7
safroniaziz <safroni.aziz@gmail.com>
2022-10-06 15:07:11 +0700

    new

--- resources/views/kepegawaian/integritas/pajak_pph.blade.php
+++ resources/views/kepegawaian/integritas/pajak_pph.blade.php
@@ -91,11 +91,7 @@
                     </nav>
                 </div>
                 <div class="col-md-12">
-                    @if ($a == "sudah")
-                        <button class="btn btn-primary btn-sm disabled"><i class="fa fa-cog fa-spin"></i>&nbsp; Generate Potongan Pajak PPH</button>
-                        @else
-                        <a href="{{ route('kepegawaian.r_integritas.generate_pph',[$periode_id]) }}" id="generate" onclick="generateTendik()" class="btn btn-primary btn-sm"><i class="fa fa-cog fa-spin"></i>&nbsp;Generate Potongan Pajak PPH</a>
-                    @endif
+                    <a href="{{ route('kepegawaian.r_integritas.generate_pph',[$periode_id]) }}" id="generate" onclick="generateTendik()" class="btn btn-primary btn-sm"><i class="fa fa-cog fa-spin"></i>&nbsp;Generate Potongan Pajak PPH</a>
                     <button class="btn btn-warning btn-sm disabled" id="proses-generate" style="display:none;color:white;cursor:pointer;"><i class="fa fa-cog fa-spin"></i>&nbsp; Generate Potongan Pajak PPH</button>
                 </div>
                 <div class="col-md-12">
