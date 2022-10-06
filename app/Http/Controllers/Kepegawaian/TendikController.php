45c1ddd7949b73f47e5ad3beea5d5d2b03e5322d
safroniaziz <safroni.aziz@gmail.com>
2022-10-06 13:33:39 +0700

    new

--- app/Http/Controllers/Kepegawaian/TendikController.php
+++ app/Http/Controllers/Kepegawaian/TendikController.php
@@ -31,12 +31,14 @@ class TendikController extends Controller
                         ->where('tendiks.nm_lengkap','like','%'.$filter.'%')
             ->orWhere('nip','like','%'.$filter.'%')
             ->orderBy('kelas_jabatan','desc')
+            ->orderBy('golongan','desc')
             ->paginate(15);
         } else
         {
             $tendiks = Tendik::leftJoin('jabatans','jabatans.id','tendiks.jabatan_id')
                             ->select('tendiks.id','nm_lengkap','nip','pangkat','golongan','nm_jabatan','jenis_kelamin','no_rekening','no_npwp')
                             ->orderBy('kelas_jabatan','desc')
+                            ->orderBy('golongan','desc')
                             ->paginate(15);
         }
         return view('kepegawaian.tendik.index',compact('tendiks','jabatans','units'));
