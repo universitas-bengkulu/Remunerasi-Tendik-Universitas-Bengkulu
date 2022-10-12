348e95067326f6afc0bc39a24f748b6251c2c82f
safroniaziz <safroni.aziz@gmail.com>
2022-10-12 11:08:13 +0700

    new

--- app/Http/Controllers/Kepegawaian/RekapitulasiController.php
+++ app/Http/Controllers/Kepegawaian/RekapitulasiController.php
@@ -318,7 +318,7 @@ class RekapitulasiController extends Controller
         $datas =  DB::table($table)->where('periode_id',$periode_id)
                     ->get();
         $akhir = DB::table($table)->select(DB::raw('sum(total_akhir_remun) as total'))->where('periode_id',$periode_id)->first();
-        return $akhir;
+        return round($akhir);
         $cek = DB::table($table)->select('total_akhir_remun')->first();
         if ($cek->total_akhir_remun != null) {
             $a = "sudah";
